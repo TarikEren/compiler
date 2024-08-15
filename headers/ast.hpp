8ec79b08a64d104d002b47ea8da3d8cd33352e1a
TarikEren <1210606015@nku.edu.tr>
2024-08-15 23:26:50 +0300

    Added function declarations for the parser tree

--- headers/ast.hpp
+++ headers/ast.hpp
@@ -1,8 +1,22 @@
-//
-// Created by MSI on 14.08.2024.
-//
-
 #ifndef COMPILER_AST_HPP
 #define COMPILER_AST_HPP
 
-#endif //COMPILER_AST_HPP
+#include "ast_node.hpp"
+#include <vector>
+
+class AST {
+private:
+    AST_Node* root;
+public:
+    AST(AST_Node* root) {
+        this->root = root;
+    }
+    void free_tree() {
+
+    }
+    void parse_tree() {
+
+    }
+};
+
+#endif
