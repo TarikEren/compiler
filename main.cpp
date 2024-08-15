8209636cd44e881d3b9aaebe4677d1c26fef442e
TarikEren <1210606015@nku.edu.tr>
2024-08-15 23:27:24 +0300

    Added TODOs, the program now takes in file name as an argument

--- main.cpp
+++ main.cpp
@@ -2,11 +2,15 @@
 #include "headers/lexer.hpp" //Lexer, Token, string, vector, util.hpp
 
 //TODOS:
-// Implement syntax checks in the lexer.
-// Check for circular includes.
-// Performance optimizations. A lot of them. Specifically adding move constructors.
-// Add argc checks and get file name from argv.
-// Create an error message function aside from the logger that takes in col and line as arguments, stringifies them and prints out a message for the user to see.
+// TODO: Check the line and col values.
+// TODO: Implement syntax checks in the lexer.
+// TODO: Implement single quotes.
+// TODO: Fix line and column counters.
+// TODO: Recognise underscores.
+// TODO: Check for circular includes.
+// TODO: Performance optimizations. A lot of them. Specifically adding move constructors.
+// TODO: Add argc checks and get file name from argv.
+// TODO: Create an error message function aside from the logger that takes in col and line as arguments, stringifies them and prints out a message for the user to see.
 
 //Possible changes:
 // The util name is kind of misleading for now. Might change it later.
@@ -15,11 +19,15 @@
 // If there isn't a space between an integer literal and a string literal, the program goes ape shit.
 // There aren't any number checks in identifiers.
 
-int main() {
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        util::log(util::ERROR, "No file provided");
+        return util::FILE_NOT_PROVIDED;
+    }
     //Initiate lexer
     Lexer lexer_obj{};
     //Read file with checking the result of the function
-    auto read_file_result = lexer_obj.read_file("test");
+    auto read_file_result = lexer_obj.read_file(argv[1]);
     //Act accordingly.
     if (read_file_result == util::FILE_NOT_FOUND) {
         return util::FILE_NOT_FOUND;
@@ -29,6 +37,7 @@ int main() {
     for (auto tok: tokens) {
         std::cout << Token::token_to_string(tok) << std::endl;
     }
-    lexer_obj.delete_tokens(tokens);
+    //TODO: Change this so that the parser takes in the tokens and deletes them as well.
+    Lexer::delete_tokens(tokens);
     return util::SUCCESS;
 }
