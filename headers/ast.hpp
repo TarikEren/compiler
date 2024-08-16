18f55a548f95982db4a7ca5040079cab72ca8f2d
TarikEren <1210606015@nku.edu.tr>
2024-08-16 18:06:12 +0300

    Added ast files

--- headers/ast.hpp
+++ headers/ast.hpp
@@ -2,21 +2,23 @@
 #define COMPILER_AST_HPP
 
 #include "ast_node.hpp"
+#include "token.hpp"
 #include <vector>
 
 class AST {
 private:
     AST_Node* root;
+    std::vector<Token*> tokens;
 public:
-    AST(AST_Node* root) {
-        this->root = root;
-    }
-    void free_tree() {
-
-    }
-    void parse_tree() {
-
-    }
+    explicit AST() = default;
+    //Provides the new_tokens to the ast.
+    void set_tokens(std::vector<Token*> new_tokens);
+    //Frees the resulting tree
+    void free_tree();
+    //Parses the provided tokens and generates an abstract syntax tree
+    void parse_tokens();
+    //Deletes the provided tokens as they are heap allocated
+    void delete_tokens();
 };
 
 #endif
--- source/ast.cpp
+++ source/ast.cpp
@@ -1,3 +1,23 @@
-//
-// Created by MSI on 16.08.2024.
-//
+#include <utility>
+
+#include "../headers/ast.hpp"
+
+void AST::delete_tokens() {
+    for (auto tok: this->tokens) {
+        delete tok;
+    }
+}
+
+void AST::free_tree() {
+
+}
+
+void AST::parse_tokens() {
+    for (auto tok: this->tokens) {
+        //Parse through the provided tokens and act upon them.
+    }
+}
+
+void AST::set_tokens(std::vector<Token *> new_tokens) {
+    this->tokens = std::move(new_tokens);
+}
